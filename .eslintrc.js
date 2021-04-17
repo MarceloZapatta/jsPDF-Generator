a4ef1edfb5e4f5d70b2f88728daef8d75d0fb1c3
Marcelo Zapatta <marcelozapatta0@gmail.com>
2021-04-17 17:26:28 -0300

    fix: eslint/prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,43 @@
 module.exports = {
   root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+  ],
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
-    // note you must disable the base rule as it can report incorrect errors
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': ['error', 'never'],
+    'quote-props': ['error', 'as-needed'],
+    quotes: ['error', 'single'],
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
+  },
+  globals: {
+    JSX: true,
   },
 };
